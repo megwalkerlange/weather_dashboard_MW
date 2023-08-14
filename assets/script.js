31a5c8aad3ad45d061e1492a6a3f9c1ad7cdcffc
Meg Walker <meg.walker56@gmail.com>
2023-08-14 13:24:58 +0100

    added weather icons

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,11 @@ function getWeather(city) {
       return response.json();
     })
     .then(function (data) {
+      icon = data.weather[0].icon;
+      var weatherIcon = `https://openweathermap.org/img/wn/` + icon + `.png`;
+
       currentDiv = $("<div>");
+
       currentName = $("<h2>").text(data.name);
       currentTemp = $("<p>").text("Temp:  " + data.main.temp + "°C");
       currentCloud = $("<p>").text(
@@ -36,11 +40,13 @@ function getWeather(city) {
       );
       currentWind = $("<p>").text("Wind Speed:  " + data.wind.speed);
       currentDate = $("<p>").text(nowDate);
+      currentIcon = $("<img>").attr("src", weatherIcon);
       // date = dayJS();
       // currentDate = date.format("DD-MM-YYYY");
 
       $("#today").append(currentDiv);
       $(currentDiv).append(currentName);
+      $(currentDiv).append(currentIcon);
       $(currentDiv).append(currentDate);
       $(currentDiv).append(currentTemp);
       $(currentDiv).append(currentCloud);
@@ -66,10 +72,12 @@ function getWeather(city) {
         // $(divContainers[]).append(futureTemp);
         divContainers[i].addClass("future-cards");
       }
-
+      icon = data.list[0].weather[0].icon;
+      var weatherIcon = `https://openweathermap.org/img/wn/` + icon + `.png`;
       future = addDate.format("DD-MM-YYYY");
 
       futureDay1 = $("<h3>").text(future);
+      futureIcon1 = $("<img>").attr("src", weatherIcon);
       futureTemp1 = $("<p>").text("Temp:   " + data.list[0].main.temp + "°C");
       futureCloud1 = $("<p>").text(
         "Cloud coverage:  " + data.list[0].weather[0].description
@@ -77,13 +85,17 @@ function getWeather(city) {
       futureWind1 = $("<p>").text("Wind Speed:  " + data.list[0].wind.speed);
 
       $(futureDiv1).append(futureDay1);
+      $(futureDiv1).append(futureIcon1);
       $(futureDiv1).append(futureTemp1);
       $(futureDiv1).append(futureCloud1);
       $(futureDiv1).append(futureWind1);
 
       future = addDate.add(1, "day").format("DD-MM-YYYY");
+      icon2 = data.list[1].weather[0].icon;
+      var weatherIcon2 = `https://openweathermap.org/img/wn/` + icon2 + `.png`;
 
       futureDay2 = $("<h3>").text(future);
+      futureIcon2 = $("<img>").attr("src", weatherIcon2);
       futureTemp2 = $("<p>").text("Temp:   " + data.list[1].main.temp + "°C");
       futureCloud2 = $("<p>").text(
         "Cloud coverage:  " + data.list[1].weather[0].description
@@ -91,13 +103,17 @@ function getWeather(city) {
       futureWind2 = $("<p>").text("Wind Speed:  " + data.list[1].wind.speed);
 
       $(futureDiv2).append(futureDay2);
+      $(futureDiv2).append(futureIcon2);
       $(futureDiv2).append(futureTemp2);
       $(futureDiv2).append(futureCloud2);
       $(futureDiv2).append(futureWind2);
 
       future = addDate.add(2, "day").format("DD-MM-YYYY");
+      icon3 = data.list[2].weather[0].icon;
+      var weatherIcon3 = `https://openweathermap.org/img/wn/` + icon3 + `.png`;
 
       futureDay3 = $("<h3>").text(future);
+      futureIcon3 = $("<img>").attr("src", weatherIcon3);
       futureTemp3 = $("<p>").text("Temp:   " + data.list[2].main.temp + "°C");
       futureCloud3 = $("<p>").text(
         "Cloud coverage:  " + data.list[2].weather[0].description
@@ -105,13 +121,17 @@ function getWeather(city) {
       futureWind3 = $("<p>").text("Wind Speed:  " + data.list[2].wind.speed);
 
       $(futureDiv3).append(futureDay3);
+      $(futureDiv3).append(futureIcon3);
       $(futureDiv3).append(futureTemp3);
       $(futureDiv3).append(futureCloud3);
       $(futureDiv3).append(futureWind3);
 
       future = addDate.add(3, "day").format("DD-MM-YYYY");
+      icon4 = data.list[3].weather[0].icon;
+      var weatherIcon4 = `https://openweathermap.org/img/wn/` + icon4 + `.png`;
 
       futureDay4 = $("<h3>").text(future);
+      futureIcon4 = $("<img>").attr("src", weatherIcon4);
       futureTemp4 = $("<p>").text("Temp:   " + data.list[3].main.temp + "°C");
       futureCloud4 = $("<p>").text(
         "Cloud coverage:  " + data.list[3].weather[0].description
@@ -119,13 +139,17 @@ function getWeather(city) {
       futureWind4 = $("<p>").text("Wind Speed:  " + data.list[3].wind.speed);
 
       $(futureDiv4).append(futureDay4);
+      $(futureDiv4).append(futureIcon4);
       $(futureDiv4).append(futureTemp4);
       $(futureDiv4).append(futureCloud4);
       $(futureDiv4).append(futureWind4);
 
       future = addDate.add(4, "day").format("DD-MM-YYYY");
+      icon5 = data.list[4].weather[0].icon;
+      var weatherIcon5 = `https://openweathermap.org/img/wn/` + icon5 + `.png`;
 
       futureDay5 = $("<h3>").text(future);
+      futureIcon5 = $("<img>").attr("src", weatherIcon5);
       futureTemp5 = $("<p>").text("Temp:   " + data.list[4].main.temp + "°C");
       futureCloud5 = $("<p>").text(
         "Cloud coverage:  " + data.list[4].weather[0].description
@@ -133,30 +157,9 @@ function getWeather(city) {
       futureWind5 = $("<p>").text("Wind Speed:  " + data.list[4].wind.speed);
 
       $(futureDiv5).append(futureDay5);
+      $(futureDiv5).append(futureIcon5);
       $(futureDiv5).append(futureTemp5);
       $(futureDiv5).append(futureCloud5);
       $(futureDiv5).append(futureWind5);
     });
 }
-//     console.log(current);
-//     return input;
-//   });
-
-// // function returnCity() {
-// //   let input = document.getElementById("search-box").value;
-
-// //   localStorage.setItem("city", input);
-
-// //   alert(input);
-
-// //   return;
-// // }
-
-// console.log(currentQueryURL);
-// console.log(futureQueryURL);
-
-// //fetch call
-
-// //set the instructions to
-
-// //within the fetch call set the code to generate card to display the necessary data
